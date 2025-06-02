3128446ef5f88b325fc04525bbaf6596b01a27c7
BECHEMIN Oscar <bechemin@info1-new.ensta.fr>
2025-06-02 21:46:13 +0200

    Description

--- Src/constante.h
+++ Src/constante.h
@@ -12,9 +12,9 @@ Contient les constantes, les includes de bibliothèques standarts et certain typ
 
     #define EXIT_FAIL 1
 
-    #define SIZE 5
-    #define DEEP 1000
-    #define NB_DESC 50000
+    #define SIZE 5 // Taille du plateau
+    #define DEEP 1000 // Nombre de coup maximal joué pour simuler une fin de partie
+    #define NB_DESC 10000 // Nombre de descentes lors d'une exploration de l'arbre
 
     typedef enum {J1, J2, BOBAIL, VIDE, B1, B2} piece_t;
     
